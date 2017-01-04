c52b9ee9bb68dbc8c865aa593e071ff7ccc6c015
jRafi <jRafi@users.noreply.github.com>
2017-01-04 22:35:50 +0100

    Add files via upload

---
+++ R Programming/cachematrix.R
@@ -0,0 +1,40 @@
+
+
+# Given a square matrix, create a special list-object which stores the functions
+# needed to calculate it's inverse.
+
+
+makeCacheMatrix <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+
+
+# Check if the inverse has already been calculated (if yes, print it).
+# Otherwise, calculate the inverse of x, cache it in m, and print m.
+
+cacheSolve <- function(x, ...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+}
+
+
+m1<-matrix(c(4,7,2,6), 2, 2)
+m2<-makeCacheMatrix(m1)
+cacheSolve(m2)
